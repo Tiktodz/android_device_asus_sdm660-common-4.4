5a1f69ab6a61749a7bb2ed3baa40633fadd2e454
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 19:28:33 +0200

    sdm660-common: Remove explicit camera HIDL interfaces from targets list
    * They're built automatically since newer revision needs them
    
    Change-Id: I695bb8d7c4120add9c8583658de5ae73f508a25c

--- sdm660.mk
+++ sdm660.mk
@@ -80,7 +80,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
