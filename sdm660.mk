3ab7424a7af3cb422d3c0c9b4dd464d89fbecbd3
pkm774 <mprabhat774@gmail.com>
2021-11-03 16:06:31 +0530

    sdm660-common: Build OSS Camera HAL
    Change-Id: I65b9eb71d6a66ef80e1cea351609c8083438e106
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -88,6 +88,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
+    camera.sdm660 \
     libpng.vendor:32 \
     vendor.qti.hardware.camera.device@1.0
 
