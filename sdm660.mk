93161f39865afa93eceb5c39ae54b1519469ed00
Michael Bestas <mkbestas@lineageos.org>
2023-10-30 17:38:30 +0200

    sdm660-common: Enforce vintf kernel requirements
    The build system enables this for shipping API level >=29
    but we also want it to pass the vintf runtime checks.
    
    Change-Id: Ie18c9a8519d9a07a2937cb0f0238776c80574955
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -10,6 +10,8 @@ $(call inherit-product, vendor/asus/sdm660-common/sdm660-common-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
+
 # Default is nosdcard, S/W button enabled in resource
 PRODUCT_CHARACTERISTICS := nosdcard
 
