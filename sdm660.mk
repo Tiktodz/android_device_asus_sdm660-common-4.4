d76d351578ab0b534ee3956933f20a698683c98a
SonicBSV <sonicbsv@gmail.com>
2024-07-29 09:01:52 +0300

    sdm660-common: Convert EGL and IMS symlinks to install_symlink targets
    Change-Id: Ie34d3917606ad9c7690a0b27d17b34d2b8b4443b
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- sdm660.mk
+++ sdm660.mk
@@ -128,8 +128,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libEGL_adreno_libEGL_adreno_symlink32 \
     libGLESv2_adreno_libGLESv2_adreno_symlink32 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink32 \
     libEGL_adreno_libEGL_adreno_symlink64 \
-    libGLESv2_adreno_libGLESv2_adreno_symlink64
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink64
 
 # Display Device Config
 PRODUCT_COPY_FILES += \
@@ -255,6 +257,10 @@ PRODUCT_COPY_FILES += \
 # Network
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64
 
 # NFC
 PRODUCT_COPY_FILES += \
