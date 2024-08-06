d46e33d8eb41d6df29d1d80786c48e7e9c1d8f81
Bruno Martins <bgcngm@gmail.com>
2024-08-06 16:45:36 +0530

    sdm660-common: Build fingerprint HAL dependency missed after U QPR3
    Change-Id: I7074078801dceff96ce206bd5c460a2dad36b6c8
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- sdm660.mk
+++ sdm660.mk
@@ -139,7 +139,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.X00TD
+    android.hardware.biometrics.fingerprint@2.1-service.X00TD \
+    liblzma.vendor
 
 # FM
 PRODUCT_PACKAGES += \
