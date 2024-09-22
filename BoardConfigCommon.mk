ea1baf0b57cc8f6626ee5fedc4643f277b37bd2c
Tiktodz <dotkit@electrowizard.me>
2024-09-22 13:10:19 +0000

    sdm660-common: Declare framework compatibility matrix
    Change-Id: I634d7816279dddefb149d4fefc02206089bc39e8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,6 +52,9 @@ TARGET_USES_ION := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# FCM
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
