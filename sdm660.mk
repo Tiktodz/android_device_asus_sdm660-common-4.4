0a83decbabe3e79d3823181de35f6d79a63cd76e
Meisie Widuri <ewprjkt@proton.me>
2024-09-23 22:47:14 +0700

    sdm660-common: Switch libqdMetaData to vendor

--- sdm660.mk
+++ sdm660.mk
@@ -116,7 +116,7 @@ PRODUCT_PACKAGES += \
     libdisplayconfig \
     libtinyxml \
     libqdMetaData \
-    libqdMetaData.system
+    libqdMetaData.vendor
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
