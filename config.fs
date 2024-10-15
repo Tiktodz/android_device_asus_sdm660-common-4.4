c247b922bae6d71920026736006019fca6d2a4f0
Tiktodz <dotkit@electrowizard.me>
2024-10-15 16:05:15 +0000

    sdm660-common: config.fs: Drop dupilcate configs
    Change-Id: I172850ce6c76f7ac3c714eb77991920dd8cf5c1d

--- config.fs
+++ config.fs
@@ -43,12 +43,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
-[vendor/bin/cnd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE NET_ADMIN BLOCK_SUSPEND
-
 [vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
 user: AID_BLUETOOTH
@@ -85,12 +79,6 @@ user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
-[vendor/bin/imsdaemon]
-mode: 0755
-user: AID_RADIO
-group: AID_RADIO
-caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
-
 [vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
@@ -122,12 +110,6 @@ group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
 [vendor/firmware_mnt/image/*]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[persist/]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
